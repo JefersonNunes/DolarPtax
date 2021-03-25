ccbf9a5c1a64a9c2206e5442ef02017e1b929d66
Jeferson Nunes <79233347+JefersonNunes@users.noreply.github.com>
2021-03-24 21:52:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 
 $url = 'https://www.bcb.gov.br/api/conteudo/pt-br/PAINEL_INDICADORES/cambio?'; //URL ONDE PEGAMOS OS DADOS
 
-$dadosite = file_get_contents($url); //PEGA TODO O CÓDIGO FONTE
-$var1 = explode('"valorVenda":',$dadosite); //DEFININDO ONDE ESTÁ O VALOR DO DOLAR PTAX
-$var2 = explode('"dataIndicador":"',$var1[1]); //REFINANDO O EXPLODE PARA TRAZER SOMENTE O QUE QUEREMOS (REMOVENDO POLUIÇÃO VISUAL)
-$Ptax = explode(',',$var2[0]); // RETORNANDO O VALOR DO DOLAR PTAX
+$dadosite = file_get_contents($url); 
+$var1 = explode('"valorVenda":',$dadosite); 
+$var2 = explode('"dataIndicador":"',$var1[1]); 
+$Ptax = explode(',',$var2[0]); 
 
 
-$var4 = explode('"dataIndicador":"',$var1[1]); //DEFININDO ONDE ESTÁ A DATA DE FECHAMENTO
-$var5 = explode('T',$var4[1]); //REFINANDO O EXPLODE PARA TRAZER SOMENTE O QUE QUEREMOS (REMOVENDO POLUIÇÃO VISUAL)
-$date = explode('-',$var5[0]); //A DATA RETORNADA DO BANCO CENTRAL VEM INVERTIDA AAAA/MM/DD, ENTÃO ESTOU DESMEMBRANDO A DATA EM PARTES
+$var4 = explode('"dataIndicador":"',$var1[1]); 
+$var5 = explode('T',$var4[1]); 
+$date = explode('-',$var5[0]); 
 
 
-print "<h1>".$Ptax[0]."</h1>"; //EXIBINDO NA TELA O VALOR RETORNADO DO DOLAR PTAX
+print "<h1>".$Ptax[0]."</h1>"; 
 
-print "<h1>Fechamento ".$date[2]."/".$date[1]."/".$date[0]."</h1>"; //EXIBINDO NA TELA O VALOR RETORNADO DA DATA DESMEMBRADA E CONCATENANDO OS VALORES NA ORDEM DD/MM/AAAA
+print "<h1>Fechamento ".$date[2]."/".$date[1]."/".$date[0]."</h1>"; 
 
 //CÓDIGO FEITO USANDO A AULA DE EXPLODE DO Rodrigo Kulb NO YOUTUBE
 ?>
